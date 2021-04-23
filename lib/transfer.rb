2a77e179c9c3261c95fff3d7d48a8e674db87b76
Cherissa Woudenberg <snerzel@comcast.net>
2021-04-23 19:24:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,7 @@ class Transfer
 
  def reverse_transfer
    if @status == "complete"
-     @sender.deposit( @amount )
+     @sender.deposit += @amount 
      @receiver.deposit( @amount * -1)
      @status = "reversed"
    end
